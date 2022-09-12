78e5129418628040a5ace3d2fa7124b5bcb5bbda
Oscar Bonilla <Oscar@gmail.com>
2022-09-12 11:10:12 -0500

    Added data to pad DB with

---
+++ UserData.sql
@@ -0,0 +1,8 @@
+INSERT INTO `capstone_db`.`accounts`
+(`user_id`,`username`,`password`,`name`,`balance`)
+VALUES
+(1,"user1","pw123","Full Name",2432.35);
+INSERT INTO `capstone_db`.`accounts`
+(`user_id`,`username`,`password`,`name`,`balance`)
+VALUES
+(2,"batLover342","bats123","Bruce Wayne",4950.90);
