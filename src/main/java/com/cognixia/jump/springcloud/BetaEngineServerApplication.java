3b31f14fd145985be45795b769d1f4ff0ea61b4c
Steven Ryan Ramos <43050949+rsteven46@users.noreply.github.com>
2022-09-12 11:38:45 -0700

    setting up project with dependenecies

---
+++ src/main/java/com/cognixia/jump/springcloud/BetaEngineServerApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetaEngineServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetaEngineServerApplication.class, args);
+	}
+
+}
