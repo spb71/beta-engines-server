5eee4c4941dfe8e515a9e93b01c4c4b99ca0a8e5
Oscar Bonilla <Oscar@gmail.com>
2022-09-13 11:42:34 -0500

    Account Controller

--- src/main/java/com/cognixia/jump/springcloud/repository/AccountsRepository.java
+++ src/main/java/com/cognixia/jump/springcloud/repository/AccountsRepository.java
@@ -1,12 +1,14 @@
 package com.cognixia.jump.springcloud.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.cognixia.jump.springcloud.model.Accounts;
 
 @Repository
 public interface AccountsRepository extends JpaRepository<Accounts, Integer>{
-	
+	@Query("select a from accounts a where a.car_id = ?1")
+	Accounts findAccountById(Integer Id);
 
 }
