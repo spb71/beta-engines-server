6bc2f525e76eeaa6d909489c8258890db8470648
Oscar Bonilla <Oscar@gmail.com>
2022-09-13 14:18:54 -0500

    Accounts model change

--- SQLFiles/InitialDatabase.sql
+++ SQLFiles/InitialDatabase.sql
@@ -4,7 +4,10 @@ create table accounts(
 	user_id int auto_increment,
 	username varchar(255) UNIQUE,
 	password varchar(255),
-	name varchar(255),
+	first_name varchar(255),
+    last_name varchar(255),
+    isEnabled varchar(255),
+    email varchar(255),
 	balance decimal(13,2),
 	primary key(user_id)
 );
