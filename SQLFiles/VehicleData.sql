88d9cfa8e3d856f40af611d18817284fa4711b19
Mohammed Sajjad <mohammedsajjad1017@gmail.com>
2022-09-15 15:23:05 -0500

    Car Insert CQL statements

--- SQLFiles/VehicleData.sql
+++ SQLFiles/VehicleData.sql
@@ -1,8 +1,26 @@
+
+
 INSERT INTO `capstone_db`.`cars`
 (`car_id`,`make`,`model`,`color`,`trim_level`,`vehicle_type`,`vehicle_year`,`vin`,`mileage`,`price`)
 VALUES
-(1,"Wayne Enterprise","Batmobile","Dark Black","Cool Trim","Chick Magnet",2013,"20202020vinnumber",42609,41.25);
+(1,"Wayne Enterprise","Batmobile","Dark Black","Cool Trim","Chick Magnet",2013,"XYK5UDA21DG367493",42069,10000000000.00);
+
 INSERT INTO `capstone_db`.`cars`
 (`car_id`,`make`,`model`,`color`,`trim_level`,`vehicle_type`,`vehicle_year`,`vin`,`mileage`,`price`)
 VALUES
-(2,"Honda","Civic","Grey","Deluxe Edition","Compact Car",1994,"20192019vinnumber",82541,1.26);
+(2,"Honda","Civic","Grey","Deluxe Edition","Compact Car",1994,"WBACB4324RFL14401",100000,0.00);
+
+INSERT INTO `capstone_db`.`cars`
+(`car_id`,`make`,`model`,`color`,`trim_level`,`vehicle_type`,`vehicle_year`,`vin`,`mileage`,`price`)
+VALUES
+(3,"Toyota","Prius","Supersonic Red","Limited","Hybrid",2022,"JH4KA7532NC036794",10000,25899.00);
+
+INSERT INTO `capstone_db`.`cars`
+(`car_id`,`make`,`model`,`color`,`trim_level`,`vehicle_type`,`vehicle_year`,`vin`,`mileage`,`price`)
+VALUES
+(4,"Lexus","LS","Blazing Red","430","Sedan",2001,"JTHBN30F210015650",185000,8995.00);
+
+INSERT INTO `capstone_db`.`cars`
+(`car_id`,`make`,`model`,`color`,`vehicle_type`,`vehicle_year`,`vin`,`mileage`,`price`)
+VALUES
+(5,"Flintmobile","BC","Orange","Prehistoric",-10000,"Fred",1151,999999999.00);
